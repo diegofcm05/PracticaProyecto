7c23b012453cb05f3ca84fd91938d90e06e62a2c
Diego Cruz <dfcm950@gmail.com>
2024-05-05 21:51:43 -0600

    primer commit

---
+++ src/practicaproyecto/PracticaProyecto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaproyecto;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class PracticaProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String x = "Diego";
+        int abc = 14;
+        
+        System.out.println("Hola mi nombre es " + x + " y tengo " + abc + " anios");
+        
+    }
+    
+}
