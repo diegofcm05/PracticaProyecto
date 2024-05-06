3f15f6536e2544cdae36932686094156821fccad
Junnior <junniorgsauceda@gmail.com>
2024-05-05 22:17:31 -0600

    Te extraño mi Osmancito

---
+++ src/practicaproyecto/TeExtrañoOsman.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicaproyecto;
+
+/**
+ *
+ * @author Junior
+ */
+public class TeExtrañoOsman {
+    String osmancito = "Vuelve Osman te extraño";
+}
